7982adf3cf166bbc334ff729cfdc735475a10819
Talis Reks <talisreks7@gmail.com>
2019-11-13 11:17:36 -0500

    Upload Radial Chart Prototype

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+// Load CSV file
+d3.csv("data/crimeData.csv", function(data){
+
+  var byCrime = d3.nest()
+      .key(function(d) { return d.OFFENSE_DESCRIPTION; })
+      .key(function(d) { return d.MONTH; })
+      .rollup(function(leaves) { return leaves.length; })
+      .entries(data);
+  console.log(byCrime)
+
+  var timeOfDay = d3.nest()
+      .key(function(d) { return d.MONTH; })
+      .rollup(function(leaves) { return leaves.length; })
+      .entries(data);
+  console.log(timeOfDay)
+
+
+  })
